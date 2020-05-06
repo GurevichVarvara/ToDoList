b7f572e38302470bca4f802a8f43e9802824d890
GurevichVarvara <gurevichvarvara@gmail.com>
2020-05-06 15:29:57 +0300

    added basic navigation

--- TODO/app.py
+++ TODO/app.py
@@ -8,5 +8,20 @@ def index():
     return render_template('todos.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/habits')
+def habits():
+    return render_template('habits.html')
+
+
+@app.route('/trash')
+def trash():
+    return render_template('trash.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)

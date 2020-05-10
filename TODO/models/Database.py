44de1764d6dceb7d11974db0cc9bb61b1057c66e
GurevichVarvara <gurevichvarvara@gmail.com>
2020-05-10 18:34:29 +0300

    added pickle serialization and deserialization

--- TODO/models/Database.py
+++ TODO/models/Database.py
@@ -1,19 +1,22 @@
 from passlib.hash import sha256_crypt
 from models.User import User
-import json
+import pickle
+import os
 
 
 class Database:
     __instance = None
 
     def __init__(self):
-        self.users = []
-        self.user_names = set()
+        self.users = {}
+
+        if os.path.getsize("db.txt") > 0:
+            with open('db.txt', 'rb') as file:
+                self.users = pickle.load(file)
 
     def _save_to_db(self):
-        with open('db.json', 'w') as f:
-            json_string = json.dumps([user.__dict__ for user in self.users])
-            json.dump(json_string, f)
+        with open('db.txt', 'wb') as file:
+            pickle.dump(self.users, file)
 
     @classmethod
     def get_instance(cls):
@@ -22,21 +25,21 @@ class Database:
         return cls.__instance
 
     def create_user(self, username, password):
-        if username in self.user_names:
+        if username in self.users.keys():
             response = 'This user name already exists'
         else:
             new_user = User(username, sha256_crypt.encrypt(password))
-            self.users.append(new_user)
-            self.user_names.add(new_user.username)
-            #self._save_to_db()
+            self.users[username] = new_user
+
+            self._save_to_db()
 
             response = id(new_user)
 
         return response
 
     def if_user_exists(self, username, password):
-        user = next((user for user in self.users if (user.username == username and
-                                                     sha256_crypt.verify(password, user.password_hash))), None)
+        if username in self.users.keys():
+            user = self.users[username] if sha256_crypt.verify(password, self.users[username].password_hash) else None
 
         response = 'Username and password combination is not valid'
         if user:

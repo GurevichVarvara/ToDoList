ced041732626498d17c289ebb672062ffbc8eb82
GurevichVarvara <gurevichvarvara@gmail.com>
2020-05-14 12:20:15 +0300

    added hidding of the adding item form after clicking on 'Cancel'

--- TODO/static/JS/add_item_form.js
+++ TODO/static/JS/add_item_form.js
@@ -1,4 +1,13 @@
-function change_add_item_form_visibility_state(state) {
+window.onload=function() {
+    /*let button_to_cancel = document.querySelector('#button_to_cancel.button_to_cancel');
+    button_to_cancel.addEventListener(change_add_item_form_visibility_state('none'));*/
+}
+
+
+function change_add_item_form_visibility_state(state, event) {
+    if (event) {
+        event.preventDefault();
+    }
     document.getElementById("gray_background").style.display = state;
     document.getElementById("add_item_form").style.display = state;
 }

496214b42a36f04bc71b547671439f367cc3ad13
GurevichVarvara <gurevichvarvara@gmail.com>
2020-05-14 11:45:07 +0300

    added displaying of the adding item form after clicking on 'Add'

---
+++ TODO/static/JS/add_item_form.js
@@ -0,0 +1,4 @@
+function change_add_item_form_visibility_state(state) {
+    document.getElementById("gray_background").style.display = state;
+    document.getElementById("add_item_form").style.display = state;
+}

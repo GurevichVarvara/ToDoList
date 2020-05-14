1b931c8aa034940f348ae2fe16b0735ad4f3c376
GurevichVarvara <gurevichvarvara@gmail.com>
2020-05-14 17:13:43 +0300

    front-end part of adding a habit fully completed. habit item html-element modified. added finish working with adding items forms

--- TODO/static/JS/add_item_form.js
+++ TODO/static/JS/add_item_form.js
@@ -1,25 +1,14 @@
 window.onload=function() {
-    let add_todo_form = document.querySelector('#add_item_form.add_item_form');
+    let add_todo_form = document.querySelector('#todo.add_item_form');
+    let add_habit_form = document.querySelector('#habit.add_item_form');
 
     if (add_todo_form) {
         add_todo_form.addEventListener('submit', add_todo);
     }
-}
 
-function clear_input(input) {
-    input.value = '';
-}
-
-function clear_checkbox(input) {
-    input.checked = false;
-}
-
-function clear_todo_inputs(title_input, category_checkboxes) {
-    category_checkboxes.forEach((checkbox) => {
-        clear_checkbox(checkbox);
-    });
-    
-    clear_input(title_input);
+    if (add_habit_form) {
+        add_habit_form.addEventListener('submit', add_habit);
+    }
 }
 
 function change_add_item_form_visibility_state(state, event) {
@@ -28,15 +17,15 @@ function change_add_item_form_visibility_state(state, event) {
     }
 
     document.getElementById("gray_background").style.display = state;
-    document.getElementById("add_item_form").style.display = state;
+    document.getElementsByClassName("add_item_form")[0].style.display = state;
 
     if (state === 'none') {
         let input_item_title = document.getElementById("item_title");
-        clear_input(input_item_title);
+        input_item_title.value = '';
 
         let input_item_days = document.getElementById("item_days");
         if (input_item_days) {
-            clear_input(input_item_days);
+            input_item_days.value = '';
         }
     }
 }
@@ -46,24 +35,55 @@ function append_todo_item_to_DOM(todo_title, category) {
     new_todo.className = "todo-item";
 
     new_todo.innerHTML = `<p class="todo-item-title">${todo_title}</p>
-                      <div class="todo-item__controls">
-                        <button class="todo-item-button todo-item-button--done">
-                          <i class="material-icons">done_outline</i>
-                        </button>
-                        <button class="todo-item-button todo-item-button--delete">
-                          <i class="material-icons">delete</i>
-                        </button>
-                      </div>`;
+                          <div class="todo-item__controls">
+                            <button class="todo-item-button todo-item-button--done">
+                              <i class="material-icons">done_outline</i>
+                            </button>
+                            <button class="todo-item-button todo-item-button--delete">
+                              <i class="material-icons">delete</i>
+                            </button>
+                          </div>`;
 
     const todo_container = document.querySelector("." + category + "-list");
     todo_container.appendChild(new_todo);
 }
 
+function append_habit_item_to_DOM(habit_title, category, periodicity) {
+    let new_habit = document.createElement('div');
+    new_habit.className = "habit";
+
+    new_habit.innerHTML = `<p class="habit-title">${habit_title}</p>
+                            <div class="habit-category">
+                                <div class="bg"></div>
+                                <div class="habit-categoty-text">${category}</div>
+                            </div>
+            
+                            <div class="completed-habit-text">
+                                <p>You can take a break from this task for ${periodicity} days</p>
+                            </div>
+            
+                            <button class="habit-done-button">
+                                <i class="material-icons">done_outline</i>
+                            </button>
+                                    
+                            <button class="habit-delete-button">
+                                <i class="material-icons">delete</i>
+                            </button>`;
+
+    const habit_container = document.getElementsByClassName("habits-container")[0];
+    habit_container.appendChild(new_habit);
+}
+
+function get_item_title() {
+    let item_title_input = document.getElementById("item_title");
+
+    return item_title_input.value;
+}
+
 function add_todo(event) {
     event.preventDefault();
 
-    let todo_title_input = document.getElementById("item_title");
-    const todo_title = todo_title_input.value;
+    const todo_title = get_item_title();
 
     let category_checkboxes = document.querySelectorAll('input[name="category_of_todo"]');
     let category = "";
@@ -75,6 +95,47 @@ function add_todo(event) {
     category = category.substring(0, category.length - 1);
     append_todo_item_to_DOM(todo_title, category);
 
-    clear_todo_inputs(todo_title_input, category_checkboxes);
+    finish_work_with_add_todo_form();
+}
+
+function add_habit(event) {
+    event.preventDefault();
+
+    const habit_title = get_item_title();
+    const habit_category = document.querySelector('input[name="category_of_habit"]:checked').value;
+    const periodicity = document.getElementById("item_days").value;
+
+    append_habit_item_to_DOM(habit_title, habit_category, periodicity);
+
+    finish_work_with_add_habit_form();
+}
+
+function clear_input_field(input) {
+    input.value = '';
+}
+
+function set_default_todo_category() {
+    let category_checkboxes = document.querySelectorAll('input[name="category_of_todo"]');
+    category_checkboxes.forEach((checkbox) => {
+        checkbox.checked = false;
+    });
+}
+
+function set_default_habit_category() {
+    document.getElementById('category_of_habit_easy').checked = true;
+}
+
+function finish_work_with_add_todo_form() {
+    clear_input_field(document.getElementById("item_title"));
+    set_default_todo_category();
+
+    change_add_item_form_visibility_state('none');
+}
+
+function finish_work_with_add_habit_form()  {
+    clear_input_field(document.getElementById("item_title"));
+    clear_input_field(document.getElementById("item_days"));
+    set_default_habit_category();
+
     change_add_item_form_visibility_state('none');
 }

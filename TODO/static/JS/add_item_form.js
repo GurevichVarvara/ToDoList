ba90883c236c999d469966c77aa599b79e90456c
GurevichVarvara <gurevichvarvara@gmail.com>
2020-05-14 13:18:42 +0300

    added clearing input fields after clicking 'cancel'

--- TODO/static/JS/add_item_form.js
+++ TODO/static/JS/add_item_form.js
@@ -3,6 +3,9 @@ window.onload=function() {
     button_to_cancel.addEventListener(change_add_item_form_visibility_state('none'));*/
 }
 
+function clear_input(input) {
+    input.value = '';
+}
 
 function change_add_item_form_visibility_state(state, event) {
     if (event) {
@@ -10,4 +13,14 @@ function change_add_item_form_visibility_state(state, event) {
     }
     document.getElementById("gray_background").style.display = state;
     document.getElementById("add_item_form").style.display = state;
+
+    if (state === 'none') {
+        let input_item_title = document.getElementById("item_title");
+        clear_input(input_item_title);
+
+        let input_item_days = document.getElementById("item_days");
+        if (input_item_days) {
+            clear_input(input_item_days);
+        }
+    }
 }
